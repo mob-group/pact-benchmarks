5e22b20b63e13510a8797d3f431cb81ae2102243
Bruce Collie <brucecollie82@gmail.com>
2019-03-14 09:05:39 +0000

    Working rename

--- benchmarks/lapack-3.8.0/rename.sh
+++ benchmarks/lapack-3.8.0/rename.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-subroutines=($(ag --nobreak --no-heading '^\s*subroutine' BLAS/SRC SRC | \
-  tr -s ' ' | cut -d' ' -f3 | cut -d '(' -f1 | sort -u))
-fns=($(ag --nobreak --no-heading '^.*FUNCTION' BLAS/SRC | tr -s ' ' | \
-  rev | cut -d' ' -f1 | rev | cut -d '(' -f1 | sort -u))
-subroutines+=("${fns[@]}")
-
 files=($(find -name '*.f'))
+
+subroutines=($(ag --nobreak --no-heading '^\h*subroutine' ${files[@]} | \
+  tr -s ' ' | cut -d' ' -f3 | cut -d '(' -f1 | sort -u))
+fns=($(ag --nobreak --no-heading '^.*FUNCTION' ${files[@]} | tr -s ' ' | \
+       cut -d'(' -f1 | rev | cut -d' ' -f1 | rev | \
+       sed 's/\.\.//g;s/arguments//g;s/argument//g' | sort -u | tail +2))
+subroutines+=("${fns[@]}")
 
 nsubs=${#subroutines[*]}
 nfiles=${#files[*]}
 ntot=$(($nsubs * $nfiles))
 
+comm=""
+for sub in ${subroutines[@]}; do
+  comm+="s/$sub/AB_$sub/Ig;"
+done
+
 i=0
-for f in ${files[@]}; do
+
+function rename_one() {
   echo -en "\r$i/$nfiles"
-  comm=""
-  for sub in ${subroutines[@]}; do
-    comm+="s/$sub/A$sub/Ig;"
-  done
-  echo $comm
+  f="$1"
   sed -i "$comm" "$f"
   ftrunc "$f" > "$f.tmp"
   mv "$f.tmp" "$f"
   i=$((i+1))
+}
+
+for f in "${files[@]}"; do
+  rename_one "$f"
 done
