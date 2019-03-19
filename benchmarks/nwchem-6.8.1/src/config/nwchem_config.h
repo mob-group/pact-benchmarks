b5d86d38817b95eba20881dc6e8a6f7c40da7deb
Bruce Collie <brucecollie82@gmail.com>
2019-03-19 15:32:22 +0000

    Reinsatll

--- benchmarks/nwchem-6.8.1/src/config/nwchem_config.h
+++ benchmarks/nwchem-6.8.1/src/config/nwchem_config.h
@@ -1,4 +1,4 @@
-# This configuration generated automatically on atocha at Fri 8 Mar 10:16:03 GMT 2019
+# This configuration generated automatically on monaco at Tue 19 Mar 15:04:45 GMT 2019
 # Request modules from user: all
 NW_MODULE_SUBDIRS = NWints atomscf ddscf gradients moints nwdft rimp2 stepper driver optim cphf ccsd vib mcscf prepar esp hessian selci dplot mp2_grad qhop property solvation nwpw fft analyz nwmd cafe space drdy vscf qmmm qmd etrans tce bq mm cons perfm dntmc dangchang ccca dimqm
 NW_MODULE_LIBS = -lccsd -lmcscf -lselci -lmp2 -lmoints -lstepper -ldriver -loptim -lnwdft -lgradients -lcphf -lesp -lddscf -ldangchang -lguess -lhessian -lvib -lnwcutil -lrimp2 -lproperty -lsolvation -lnwints -lprepar -lnwmd -lnwpw -lofpw -lpaw -lpspw -lband -lnwpwlib -lcafe -lspace -lanalyze -lqhop -lpfft -ldplot -ldrdy -lvscf -lqmmm -lqmd -letrans -lpspw -ltce -lbq -lmm -lcons -lperfm -ldntmc -lccca -ldimqm
