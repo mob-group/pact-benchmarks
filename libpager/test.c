86dbce5615c2e01ae3b45ccd1eb6c374f0f72802
Bruce Collie <brucecollie82@gmail.com>
2019-03-25 15:28:05 +0000

    Not working if table full

--- libpager/test.c
+++ libpager/test.c
@@ -4,12 +4,31 @@
 #include <stdlib.h>
 
 int main(void) {
+  for (int i = 0; i < 11; ++i) {
+    int *h = malloc(sizeof(int));
+    *h = 1;
+
+    int *d = malloc(sizeof(int));
+
+    new_table_entry(h, d, sizeof(int), heap_functions);
+
+    for (int i = 0; i < 8; ++i) {
+      printf("h:%p d:%p\n", table.entries[i].host, table.entries[i].device);
+    }
+    printf("\n");
+  }
+
   int *h = malloc(sizeof(int));
   *h = 4;
 
   int *d = malloc(sizeof(int));
 
   new_table_entry(h, d, sizeof(int), heap_functions);
+  for (int i = 0; i < 8; ++i) {
+    printf("h:%p d:%p\n", table.entries[i].host,
+           get_table_entry(table.entries[i].host));
+  }
+  printf("\n");
 
   *h = 2;
   printf("%d\n", *(int *)get_table_entry(h));
