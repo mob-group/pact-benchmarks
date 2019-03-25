0c37115e7d5eeb8b53e014a384b0b5fdd0695306
Bruce Collie <brucecollie82@gmail.com>
2019-03-25 14:02:07 +0000

    Rewrite some

--- libpager/pager.c
+++ libpager/pager.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 
 void do_nothing(UNUSED void *ptr) {}
 
@@ -17,28 +18,44 @@ page_functions_st stack_functions = {.deleter = (deleter_ft)do_nothing,
                                      .copier = (copier_ft)memcpy};
 
 void new_table_entry(void *h, void *d, size_t s, page_functions_st fns) {
+  // Logic:
+  //  - if there is an entry in the table with the same host pointer as this
+  //    one, then update it and delete whatever was there before
+  //  - if there is an empty slot, then insert there
+  //  - otherwise, delete whatever is at next insertion point and move the
+  //    pointer along one.
+  //  - in every case, we finish by updating one entry with a new struct
+  //  - because we initially fill it up in order, then if we find an empty entry
+  //    we know that there can't be one to overwrite before it.
+
+  size_t insert_point = table.next_insert;
+  bool must_delete = true;
+
   for (size_t i = 0; i < table.size; ++i) {
-    if (table.entries[i].host == h) {
-      table.entries[i].functions.deleter(table.entries[i].device);
-      table.next_insert = i;
+    page_entry_st e = table.entries[i];
+    if (!e.host || e.host == h) {
+      insert_point = i;
+      must_delete = !!e.host;  // don't delete whatever's there if it was NULL
+      break;
     }
   }
 
-  // Handle the case where the table is full and we need to delete something
-  // here (by deleting).
-  size_t ni = table.next_insert;
+  page_entry_st old_e = table.entries[insert_point];
+  if (must_delete) {
+    old_e.functions.deleter(old_e.device);
+  }
 
-  table.entries[ni].host = h;
-  table.entries[ni].device = d;
-  table.entries[ni].last_size = s;
-  table.entries[ni].written = false;
-  table.entries[ni].functions = fns;
-
-  // Also mprotect here to track whether the host gets written or not
-
+  page_entry_st new_e = {.host = h,
+                         .device = d,
+                         .last_size = s,
+                         .written = false,
+                         .functions = fns};
+  table.entries[insert_point] = new_e;
   fns.copier(d, h, s);
 
-  table.next_insert = (ni + 1) % table.size;
+  // mprotect host pointer now
+
+  table.next_insert = (table.next_insert + 1) % table.size;
 }
 
 void *get_table_entry(void *h) {
@@ -47,6 +64,7 @@ void *get_table_entry(void *h) {
       if (table.entries[i].written) {
         table.entries[i].functions.copier(table.entries[i].device, h,
                                           table.entries[i].last_size);
+        table.entries[i].written = false;
       }
       return table.entries[i].device;
     }
--- libpager/test.c
+++ libpager/test.c
@@ -3,4 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {}
+int main(void) {
+  int h = 4;
+  int d;
+
+  new_table_entry(&h, &d, sizeof(int), stack_functions);
+  printf("%d\n", *(int *)get_table_entry(&h));
+}
