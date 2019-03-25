5a57f48a76d39ff788cfdac0104998a55a1ce5c0
Bruce Collie <brucecollie82@gmail.com>
2019-03-25 16:30:31 +0000

    Abinit cublas paged

--- benchmarks/abinit/build-cublas.sh
+++ benchmarks/abinit/build-cublas.sh
@@ -7,5 +7,5 @@
     --with-linalg-libs="-L$(realpath ..)/lapack-mkl/build/lib \
                         -L$PACT_BENCH/local/lib64 -L$PACT_BENCH/local/lib \
                         -llapack -lblas \
-                        -lcuda -lcublas -lcudart -lpthread -ldl -lcudashim"
+                        -lcuda -lcublas -lcudart -lpthread -ldl -lcudashim -lpager"
 make multi multi_nprocs=$(nproc)
