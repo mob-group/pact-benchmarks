78038fbaea7fa1ca1cdc2b017e52fd2f0c53b6fa
Bruce Collie <brucecollie82@gmail.com>
2019-03-27 08:44:18 +0000

    Draw an actual bar

--- results/make_bars.py
+++ results/make_bars.py
@@ -20,20 +20,34 @@ BarData.__new__.__defaults__ = (None,) * 2
 
 data = [
     [
-        BarData('Abinit', 'Ti22', 1.2, 'MKL')
+        BarData('Abinit', 'Ti22', 1.2, 'MKL', 1.4, 'MKL')
     ]
 ]
 
+def has_expert(data):
+    return None not in [data.exp_speed, data.exp_impl]
+
+def best_speed(row):
+    return max([max(d.exp_speed, d.our_speed) for d in row])
+
 # Take an axis and some bar data, then plot data on the axis
-def bar(ax, data):
-    print(data)
+def bar(ax, data, y_max):
+    bar_params = { 'width' : 0.8 }
+
+    ax.bar(-1, 1, **bar_params)
+    ax.bar(0, data.our_speed, **bar_params)
+    if has_expert(data):
+        ax.bar(1, data.exp_speed, **bar_params)
+
+    ax.set_xlim(-1.5, 1.5)
+    ax.set_ylim(0, y_max * 1.1)
 
 if __name__ == "__main__":
-    fig, axes = plt.subplots(2, 5, sharey=True, figsize=(7,3))
+    fig, axes = plt.subplots(2, 5, sharey='row', figsize=(7,3))
 
     for ax_row, data_row in zip(axes, data):
         for pair in zip(ax_row, data_row):
-            bar(*pair)
+            bar(*pair, best_speed(data_row))
 
     fig.tight_layout()
     plt.savefig('out.pdf')
