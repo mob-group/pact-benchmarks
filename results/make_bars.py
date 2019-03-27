c78f35acff730c0ae419d3627856cebbbe99f9da
Bruce Collie <brucecollie82@gmail.com>
2019-03-27 15:57:31 +0000

    ABINIT placeholders

--- results/make_bars.py
+++ results/make_bars.py
@@ -24,10 +24,13 @@ BarData.__new__.__defaults__ = (None,) * 2
 
 data = [
     [
-        BarData('Abinit', 'Ti22', 1.2, 'MKL', 1.4, 'MKL')
+        BarData('', 'ResNet-152', 9.362466855, 'MKL', 48.62573943, ''),
+        BarData('', 'VGG-16', 10.85887595, 'MKL', 61.97973289, ''),
+        BarData('', 'DenseNet-201', 5.609201284, 'MKL', 21.76856879, ''),
     ],
     [
-        BarData('', 'VGG-16', 2.2, 'CUDA', 2.8, 'CUDA')
+        BarData('Abinit', 'Ti22', 1, 'MKL', 1, ''),
+        BarData('Abinit', 'V402', 1, 'MKL', 1, ''),
     ]
 ]
 
@@ -46,30 +49,40 @@ def expert_label(data):
 # Take an axis and some bar data, then plot data on the axis
 def bar(ax, data, y_max):
     bar_params = { 'width' : 0.8 }
+    ret = []
 
-    ax.bar(-1, 1, **bar_params)
-    ax.bar(0, data.our_speed, **bar_params)
+    ret.append(ax.bar(-1, 1, **bar_params))
+    ret.append(ax.bar(0, data.our_speed, **bar_params))
     if has_expert(data):
-        ax.bar(1, data.exp_speed, **bar_params)
+        ret.append(ax.bar(1, data.exp_speed, **bar_params))
+        if data.exp_speed > (data.our_speed * 2.5):
+            ax.text(1, data.our_speed * 2.5, '{:.0f}x  '.format(data.exp_speed),
+                    rotation=90, fontsize=6, va='top', ha='center',
+                    color='white')
 
     ax.set_xlim(-1.5, 1.5)
-    ax.set_ylim(0, y_max * 1.1)
+    ax.set_ylim(0, data.our_speed * 2.5)
 
     ax.axhline(y=1, color='black', lw=0.8)
 
     ax.set_xticks([-1, 0, 1])
-    ax.set_xticklabels(['Base', '{}'.format(data.our_impl), expert_label(data)])
+    # ax.set_xticklabels(['Base', '{}'.format(data.our_impl), expert_label(data)])
+    ax.set_xticklabels([])
     ax.tick_params(axis='x', labelsize=8)
 
     ax.set_title('{} {}'.format(data.benchmark, data.dataset))
+    return ret
 
 if __name__ == "__main__":
-    fig, axes = plt.subplots(2, 5, sharey='row', figsize=(7,3))
+    fig, axes = plt.subplots(2, 5, sharey=False, figsize=(7,3))
 
     for ax_row, data_row in zip(axes, data):
         for pair in zip(ax_row, data_row):
-            bar(*pair, best_speed(data_row))
+            legs = bar(*pair, best_speed(data_row))
 
+    fig.legend(legs, ['Baseline', 'Ours', 'Expert'],
+            bbox_to_anchor=(0.5, 0), loc='lower center', ncol=3)
     fig.tight_layout()
+    fig.subplots_adjust(bottom=0.2)
     sns.despine(fig)
     plt.savefig('perf.pdf')
