20840e2b69a0eb7c5429546f0ffcf5fb6960d7b8
Bruce Collie <brucecollie82@gmail.com>
2019-03-29 10:27:33 +0000

    Empty sgemm

--- libclblastshim/clblastshim.h
+++ libclblastshim/clblastshim.h
@@ -1,1 +1,11 @@
-void test();
+extern "C" {
+
+void blast_sgemm_(
+  char const* transA, char const* transB,
+  int m, int n, int k, float alpha,
+  float *A, int lda,
+  float *B, int ldb,
+  float *C, int ldc
+);
+
+}
--- libclblastshim/lib.cpp
+++ libclblastshim/lib.cpp
@@ -1,5 +1,15 @@
 #include <clblast.h>
 
-void test()
+extern "C" {
+
+void blast_sgemm_(
+  char const* transA, char const* transB,
+  int m, int n, int k, float alpha,
+  float *A, int lda,
+  float *B, int ldb,
+  float *C, int ldc
+)
 {
+}
+
 }
